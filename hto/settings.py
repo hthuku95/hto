66c81fb1b513fc967a0770216d4225465565ba7d
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-11-22 04:44:03 +0300

    updated IP

--- hto/settings.py
+++ hto/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-3*=c$5loiq3^08*#q#kz!nk0b5yee%ehcf@8wyh2pl%)j=b)jv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['147.182.200.51','htostudios.com','www.htostudios.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['147.182.215.87','htostudios.com','www.htostudios.com','localhost','127.0.0.1']
 
 
 # Application definition
