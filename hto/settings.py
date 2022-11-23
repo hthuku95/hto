99085cd4b5725b37661d444b0fd533f8b0484d33
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-11-23 06:01:02 +0300

    renamed te static folder

--- hto/settings.py
+++ hto/settings.py
@@ -138,9 +138,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'static_in_env'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
