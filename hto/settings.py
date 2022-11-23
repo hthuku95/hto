5aac485ce742d8368939f70b470b9736afb2e173
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-11-23 09:33:37 +0300

    removed static_root

--- hto/settings.py
+++ hto/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
