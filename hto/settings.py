ddb6917f3a1d793afd950469e8f6e6716f2c0ba3
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-11-23 08:34:41 +0300

    debug set to false

--- hto/settings.py
+++ hto/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3*=c$5loiq3^08*#q#kz!nk0b5yee%ehcf@8wyh2pl%)j=b)jv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','157.245.250.50']
 
