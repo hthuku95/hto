591e154ea3ac92cb1e951b84009a08aa37da832f
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-11-27 12:24:46 +0300

    Updated the article details URL

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path(r'',views.article_list, name= 'article_lists'),
-    path(r'<slug>/',views.article_details, name='article_details'),
+    path(r'<year>/<month>/<day>/<slug>/',views.article_details, name='article_details'),
     path(r'category/<slug>/',views.article_category, name= 'article_category'),
 ]
