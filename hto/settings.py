ecdd734b9cc9636df467b22ae657c163635b61b4
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-12-06 13:33:31 +0300

    updated allowed hosts

--- hto/settings.py
+++ hto/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost',env('PRODUCTION_IP')]
+ALLOWED_HOSTS = [env('PRODUCTION_IP'),'127.0.0.1','localhost']
 
 
 # Application definition
