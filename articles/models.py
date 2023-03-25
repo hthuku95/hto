6f5817cdfda6ce9cf230b4b0b1e22fc8724aa341
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-25 11:59:05 +0300

    Updated maximum chars

--- articles/models.py
+++ articles/models.py
@@ -35,22 +35,22 @@ class Tag(models.Model):
 # article model
 class Article (models.Model):
     title = models.CharField( max_length=512 ,blank=True)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=512)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     date = models.DateTimeField(  auto_now_add=True)
 
     views = models.IntegerField(blank=True,default=0,null=True)
 
     thumb = models.FileField(blank=True,null=True)
-    thumb_description = models.CharField( max_length=50,null=True) 
+    thumb_description = models.CharField( max_length=512,null=True) 
 
     # Populate the DB with many tags and rotate them 
     tags = models.ManyToManyField(Tag, blank=True)
 
     category = models.ForeignKey(Category, blank=True,null=True, on_delete=models.SET_NULL)
-    summary = models.TextField(max_length=512, default="Article summary")
-    intro = models.TextField( max_length=512, blank=True, null=True)
-    outro = models.TextField( max_length=512, blank=True, null=True)
+    summary = models.TextField(default="Article summary")
+    intro = models.TextField(blank=True, null=True)
+    outro = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -69,7 +69,7 @@ class Article (models.Model):
 # sections model
 class Section (models.Model):
     root_article = models.ForeignKey(Article,on_delete=models.CASCADE,null=True)
-    heading = models.CharField(blank=True,null=True, max_length=50)
+    heading = models.CharField(blank=True,null=True, max_length=512)
     text_body = models.TextField(blank=True)
     quote = models.TextField(blank=True,null=True)
     code = models.TextField(blank=True,null=True)
