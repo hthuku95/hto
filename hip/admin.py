7634d26729635298d8e37988ec621107c1643adb
HARRY OMOLLOH <hthuku95@gmail.com>
2023-04-20 09:49:05 +0300

    Registered subtitle model

--- hip/admin.py
+++ hip/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ImprovementProposal
+from .models import ImprovementProposal,ArticleIdea,SubTitle
 # Register your models here.
 
 
 admin.site.register(ImprovementProposal)
+admin.site.register(ArticleIdea)
+admin.site.register(SubTitle)
