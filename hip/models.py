dbbb1ab1fb77c11dc0f1cedf7494de1a1c804d0c
HARRY OMOLLOH <hthuku95@gmail.com>
2023-04-26 19:38:44 +0300

    fixed the description bug

--- hip/models.py
+++ hip/models.py
@@ -31,7 +31,7 @@ class ImprovementProposal(models.Model):
     
 class ArticleIdea(models.Model ):
     title = models.CharField(max_length=256)
-    description = TextField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
 
     def __str__(self):
         return self.title
