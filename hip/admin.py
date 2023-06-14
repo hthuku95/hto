8dadfc45a369fae6f07bfce3f9e23cef9820af25
HARRY OMOLLOH <hthuku95@gmail.com>
2023-06-14 17:12:51 +0300

    Removed cache

--- hip/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ImprovementProposal,ArticleIdea
-# Register your models here.
-
-
-admin.site.register(ImprovementProposal)
-admin.site.register(ArticleIdea)
