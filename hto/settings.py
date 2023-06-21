a0ddf5f271d32dddb99009fa98f22b57ffb945ee
HARRY OMOLLOH <hthuku95@gmail.com>
2023-06-21 14:46:35 +0300

    Added production IP and Domain names

--- hto/settings.py
+++ hto/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost',env('PRODUCTION_IP'),'htostudios.com','www.htostudios.com']
 
 
 # Application definition
